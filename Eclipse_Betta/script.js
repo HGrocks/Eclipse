f2256ebed9af6b4dd7f0382cd674c5fbbd270f9e
HGrocks <167364933+HGrocks@users.noreply.github.com>
2024-05-29 17:19:53 -0300

    Projeto Eclipse
    Adicionado as páginas do projeto e suas complementações.

---
+++ Eclipse_Betta/script.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', () => {
+    document.getElementById('header').classList.add('visible');
+    document.querySelector('.login-container').classList.add('visible');
+});
